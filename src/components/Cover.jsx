abd74dfba3a93f1ab70d7653bc075859a8646932
Erick Michell <michellerik96@gmail.com>
2026-09-22 19:58:58 -0600

    Agregar Decoraciones 5.5

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -223,7 +223,6 @@ export default function Cover({ onOpen }) {
                         ✦
                     </motion.span>
 
-
                     <h1>Camila</h1>
 
                     <motion.span
